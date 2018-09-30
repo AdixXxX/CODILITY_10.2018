1b3ec364f6da62b8cfb6d3678a80c88f1422eda9
AdixXxX <adrianlachowicz@o2.pl>
2018-09-30 22:15:37 +0200

    first commit

---
+++ src/Osoba.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Osoba
+{
+    public int Wiek;
+    public String Imie;
+
+    public int getWiek() {
+        return Wiek;
+    }
+
+    public void setWiek(int wiek) {
+        Wiek = wiek;
+    }
+
+    public String getImie() {
+        return Imie;
+    }
+
+    public void setImie(String imie) {
+        Imie = imie;
+    }
+
+    public Osoba(int wiek, String imie)
+    {
+        this.Wiek = wiek;
+        this.Imie = imie;
+    }
+}
---
+++ src/Task_1.java
@@ -0,0 +1,40 @@
+public class Task_1
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {1,1,1,1,0,0,0,0};
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(int[] A)
+    {
+        int reverseCounter = 0;
+        int heads = 0;
+        int tails = 0;
+
+        for (int i = 0; i <A.length ; i++)
+        {
+          if(A[i] == 0)
+          {
+              tails++;
+          }else
+          {
+              heads++;
+          }
+        }
+
+        if(tails>heads)
+        {
+            reverseCounter = heads;
+        }
+        else if(heads>tails)
+        {
+            reverseCounter = tails;
+        }
+        else if(heads==tails)
+        {
+            reverseCounter = tails;
+        }
+        return reverseCounter;
+    }
+}
